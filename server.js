d4b9535b1554d8d5bc17c950778f2879c4e2f556
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-05-19 12:07:11 -0500

    Created routes for webpages

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -6,7 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Routes
+app.get('/', (req, res) => {
+    const HTMLfile = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(HTMLfile);
+});
 
+app.get('/register-page', (req, res) => {
+    const registerPage = path.join(__dirname, 'public', 'register.html');
+    res.sendFile(registerPage);
+});
+
+app.post('/login', (req, res) => {
+    
+});
+
+app.post('/register', (req, res) => {
+    
+});
 
 // Start the server
 app.listen(PORT, () => {
